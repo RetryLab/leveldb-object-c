614897d4055725d373c6da420d32f862c958388f
Tad Wang <wptad@tom.com>
2013-11-03 23:14:35 +0800

    update

--- leveldb-object-c/LevelDB.mm
+++ leveldb-object-c/LevelDB.mm
@@ -11,26 +11,24 @@
 #import <leveldb/options.h>
 
 #define SliceFromString(_string_) (Slice((char *)[_string_ UTF8String], [_string_ lengthOfBytesUsingEncoding:NSUTF8StringEncoding]))
-#define StringFromSlice(_slice_) ([[[NSString alloc] initWithBytes:_slice_.data() length:_slice_.size() encoding:NSUTF8StringEncoding] autorelease])
+#define StringFromSlice(_slice_) ([[NSString alloc] initWithBytes:_slice_.data() length:_slice_.size() encoding:NSUTF8StringEncoding] )
 
 
 using namespace leveldb;
 
 static Slice SliceFromObject(id object) {
-    NSMutableData *d = [[[NSMutableData alloc] init] autorelease];
+    NSMutableData *d = [[NSMutableData alloc] init];
     NSKeyedArchiver *archiver = [[NSKeyedArchiver alloc] initForWritingWithMutableData:d];
     [archiver encodeObject:object forKey:@"object"];
     [archiver finishEncoding];
-    [archiver release];
     return Slice((const char *)[d bytes], (size_t)[d length]);
 }
 
 static id ObjectFromSlice(Slice v) {
     NSData *data = [NSData dataWithBytes:v.data() length:v.size()];
     NSKeyedUnarchiver *unarchiver = [[NSKeyedUnarchiver alloc] initForReadingWithData:data];
-    id object = [[unarchiver decodeObjectForKey:@"object"] retain];
+    id object = [unarchiver decodeObjectForKey:@"object"];
     [unarchiver finishDecoding];
-    [unarchiver release];
     return object;
 }
 
@@ -75,8 +73,18 @@ static id ObjectFromSlice(Slice v) {
 
 + (LevelDB *)databaseInLibraryWithName:(NSString *)name {
     NSString *path = [[LevelDB libraryPath] stringByAppendingPathComponent:name];
-    LevelDB *ldb = [[[LevelDB alloc] initWithPath:path] autorelease];
+    LevelDB *ldb = [[LevelDB alloc] initWithPath:path];
     return ldb;
+}
+
+- (void) putString:(id)value forKey:(NSString *)key {
+    Slice k = SliceFromString(key);
+    Slice v = SliceFromString(value);
+    Status status = db->Put(writeOptions, k, v);
+    
+    if(!status.ok()) {
+        NSLog(@"Problem storing key/value pair in database: %s", status.ToString().c_str());
+    }
 }
 
 - (void) putObject:(id)value forKey:(NSString *)key {
@@ -134,7 +142,7 @@ static id ObjectFromSlice(Slice v) {
 }
 
 - (NSArray *)allKeys {
-    NSMutableArray *keys = [[[NSMutableArray alloc] init] autorelease];
+    NSMutableArray *keys = [[NSMutableArray alloc] init];
     //test iteration
     [self iterateKeys:^BOOL(NSString *key) {
         [keys addObject:key];
--- leveldb-object-c/main.m
+++ leveldb-object-c/main.m
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "LevelDB.h"
 
 int main(int argc, const char * argv[])
 {
@@ -14,7 +15,19 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Leveldb Put and Get Test.....");
+        
+        
+        LevelDB *ldb = [LevelDB databaseInLibraryWithName:@"test.ldb"];
+        
+        //test string
+        [ldb putObject:@"laval" forKey:@"string_test"];
+        NSLog(@"String Value: %@", [ldb getString:@"string_test"]);
+        
+        //test dictionary
+        [ldb putObject:[NSDictionary dictionaryWithObjectsAndKeys:@"val1", @"key1", @"val2", @"key2", nil] forKey:@"dict_test"];
+        NSLog(@"Dictionary Value: %@", [ldb getDictionary:@"dict_test"]);
+//        [super viewDidLoad];
         
     }
     return 0;
