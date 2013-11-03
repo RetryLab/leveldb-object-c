69319c0cbd8ba7c0498d510a35469d37375bff9e
Tad Wang <wptad@tom.com>
2013-11-03 22:06:32 +0800

    Initial Commit

---
+++ leveldb-object-c/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  leveldb-object-c
+//
+//  Created by Tad Wang on 11/3/13.
+//  Copyright (c) 2013 mountdoom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
